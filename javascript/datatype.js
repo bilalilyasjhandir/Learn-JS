921d877aa448b2ab58592ef961f0ce36bd369cb8
Bilal Ilyas Jhandir <152180151+bilalilyasjhandir@users.noreply.github.com>
2024-08-02 19:07:50 +0000

    datatypes in javascript

---
+++ javascript/datatype.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS code as newer version
+
+console.log( 3            
+    + 3                   //not a good way to write code
+) 
+
+console.log("Data Types")
+
+let name = "Bilal"     //string => '' or ""
+let age = 20           //int //can accept numbers => 2 to power 53 // for more big number use bigint
+let isLoggedIn = true  //boolean => true or false
+let currentState;      //undefinefd value
+let attendance = null  //standalone value
+
+//symbol => unique
+//object
+
+console.log(typeof undefined)  //undefined
+console.log(typeof null)       //object
