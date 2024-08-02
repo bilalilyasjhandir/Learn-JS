74cf11a584fffeba2f527d8da24c557c992f890a
Bilal Ilyas Jhandir <152180151+bilalilyasjhandir@users.noreply.github.com>
2024-08-02 16:09:52 +0000

    variable in js

---
+++ javascript/variable.js
@@ -0,0 +1,18 @@
+const userId = 1234
+let userEmail = "bilal@email.com"
+var userPassword = "123"
+userCity = "Bahawalpur"
+
+/*
+do not use var, use let
+*/
+
+// userId = 12 // not allowed
+
+userEmail = "bilal123@email.com"
+userPassword = "1234"
+userCity = "Lahore"
+
+console.log(userId);
+
+console.table([userId, userEmail, userPassword, userCity])
