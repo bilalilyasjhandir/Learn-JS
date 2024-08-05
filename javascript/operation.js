1b961f53663b567ea17bc25f6104f38ebe58882d
Bilal Ilyas Jhandir <bilaljhandir007@gmail.com>
2024-08-05 22:34:00 +0500

    different operations

---
+++ javascript/operation.js
@@ -0,0 +1,28 @@
+let value = 5
+console.log(-value)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " World"
+console.log(str1 + str2)
+
+console.log(1 + "2")
+console.log(1 + 2 + "3")
+console.log("1" + 2 + 3)
+
+console.log(+true)
+console.log(+"")
+
+let x = 3
+let y = x++
+console.log(`x:${x}, y:${y}`)
+
+let a = 3
+let b = ++a
+console.log(`a:${a}, b:${b}`)
