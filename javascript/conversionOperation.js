c3d2051116e8790fceacf7652fb2b31c57861c41
Bilal Ilyas Jhandir <bilaljhandir007@gmail.com>
2024-08-05 22:08:48 +0500

    conversion operations on different data types

---
+++ javascript/conversionOperation.js
@@ -0,0 +1,68 @@
+//Number Operation for a number
+let num = "20"
+console.log(typeof num)
+
+let numValue = Number(num)
+console.log(typeof numValue)
+console.log(numValue)
+
+//Number operation for a string
+let numWithLetter = "20abc"
+console.log(typeof numWithLetter)
+
+let numLetterValue = Number(numWithLetter)
+console.log(typeof numLetterValue)
+console.log(numLetterValue)
+
+//Number operation for a null
+let nothing = null
+console.log(typeof nothing)
+
+let nothingInNum = Number(nothing)
+console.log(typeof nothingInNum)
+console.log(nothingInNum)
+
+//Number operation for an undefined
+let notDefined = undefined
+console.log(typeof notDefined)
+
+let undefinedInNum = Number(notDefined)
+console.log(typeof undefinedInNum)
+console.log(undefinedInNum)
+
+//Number operation for a boolean
+let boolValue = true
+console.log(typeof boolValue)
+
+let boolInNum = Number(boolValue)
+console.log(typeof boolInNum)
+console.log(boolInNum)
+
+//Number operation for a string
+let name = "Bilal"
+console.log(typeof name)
+
+let nameInNum = Number(name)
+console.log(typeof nameInNum)
+console.log(nameInNum)
+
+//Boolean operation on a number
+let loggedIn = 1
+let numInBool = Boolean(loggedIn)
+console.log(loggedIn)
+
+//Boolean operation on a empty string
+let emptyString = ""
+let emptyInBool = Boolean(emptyString)
+console.log(emptyInBool)
+
+//Boolean operation on a string
+let myName = "Bilal"
+let nameInBool = Boolean(myName)
+console.log(nameInBool)
+
+//String operation on a number
+let someNum = 30
+let numInString = String(someNum)
+console.log(numInString)
+console.log(typeof numInString)
