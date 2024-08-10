705ebf6bb43f0ab8f6b3d940e200a642c3e42d84
Bilal Ilyas Jhandir <bilaljhandir007@gmail.com>
2024-08-10 22:57:20 +0500

    object in js

--- javascript/object.js
+++ javascript/object.js
@@ -41,3 +41,54 @@ student.myName = function(){
 
 console.log(student.greeting());
 console.log(student.myName());
+
+
+//singleton object
+const user1 = Object()
+console.log(user1);
+
+//non-singleton object 
+const user2 = {}
+console.log(user2);
+
+user1.id = "1001"
+user1.name = "Bilal"
+user1.fullname = {
+    userFullname: {
+        firstName: "Bilal",
+        midName: "Ilyas",
+        lastName: "Jhandir"
+    }
+}
+
+console.log(user1.fullname.userFullname);
+
+//combine two or more objects
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
+
+//does not work properly
+const obj4 = {obj1, obj2}
+console.log(obj4);
+
+//works properly
+const obj5 = Object.assign({}, obj1, obj2, obj3)
+console.log(obj5);
+
+//more easy method
+const obj6 = {...obj1, ...obj2, ...obj3}
+console.log(obj6);
+
+const users = [
+    {
+        //objects within an array
+    }
+]
+
+console.log(Object.keys(user1));
+console.log(Object.values(user1));
+console.log(Object.entries(user1));
+
+console.log(user1.hasOwnProperty('id'));
+console.log(user1.hasOwnProperty('mobile'));
