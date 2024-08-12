c37df8e8654274c6d15ea25a1cf2e3a4c1365960
Bilal Ilyas Jhandir <bilaljhandir007@gmail.com>
2024-08-13 00:04:38 +0500

    iife in js

--- javascript/iife.js
+++ javascript/iife.js
@@ -1,9 +1,16 @@
 //Immediately Invoked Function Expressions
 //Syntax: 1st () for expression, 2nd () for execution
+
+//named IIFE
 (function one(){
     console.log("Hello World");  
 })(); //must add ;
 
+//unnamed IIFE
 ( () => {
     console.log("Hello World"); 
-} )()
+} )();
+
+( (name) => {
+    console.log(`Hello ${name}`); 
+} )("Bilal")
