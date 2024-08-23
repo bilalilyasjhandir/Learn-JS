d9f854d66465fa7f8fffe87cb2a95713f613a073
Bilal Ilyas Jhandir <bilaljhandir007@gmail.com>
2024-08-23 16:34:20 +0500

    oop in js

---
+++ javascript/oop.js
@@ -0,0 +1,33 @@
+const user = {
+    name: "Bilal",
+    age: 20,
+    isLoggedIn: true,
+    getUserDetails: function(){
+        console.log("Got all the user details");
+        console.log(`Username: ${this.name}`);
+        console.log(this);
+        
+    }
+}
+
+console.log(user.name);
+console.log(user.getUserDetails());
+console.log(this);
+
+function User(name, age, isLoggedIn){
+    this.name = name,
+    this.age = age,
+    this.isLoggedIn = isLoggedIn
+    return this
+}
+
+const userOne = User("Ali", 25, true)
+const userTwo = User("Ahmad", 30, false)
+console.log(userOne); //will give the details of userTwo //use new keyword to avoid overwritting
+
+const userThree = new User("Dawood", 35, true)
+const userFour = new User("Shahid", 40, false)
+console.log(userThree);
+
+
+console.log(userThree.constructor);
